9e4d0ead96acf5cdd808578e84813e0253cd1f77
yangjun <1661512152@qq.com>
2021-06-23 07:33:18 +0800

    hot-fix commit

---
+++ src/main/java/com/yangjun/test/bean/User.java
@@ -0,0 +1,18 @@
+package com.yangjun.test.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class User {
+
+    private int id;
+
+    private String name;
+
+    private String address;
+
+}
